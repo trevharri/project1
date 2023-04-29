2d540a68037f267c399c8b78075586dfd8238db9
trevharri <trevor.harri94@gmail.com>
2023-04-29 20:46:01 +0200

    first deploy

---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Article from '../../components/article/Article'
+import './blog.css';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
+
+const articleInfo = {
+  date: "Sep 26, 2021",
+  title: "GPT-3 and Open  AI is the future. Let us exlore how it is?"
+}
+
+const imagesB = [blog02, blog03, blog04, blog05]
+
+
+const Blog = () => {
+  return (
+    <div className='gpt3__blog section__padding' id="blog" >
+      <div className="gpt3__blog-heading">
+        <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container__groupA">
+          <Article imgUrl={blog01} date={articleInfo.date} title={articleInfo.title} />
+        </div>
+        <div className="gpt3__blog-container__groupB">
+          {imagesB.map((img, index) => {
+            return <Article imgUrl={img} date={articleInfo.date} title={articleInfo.title} key={index} />
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Blog
